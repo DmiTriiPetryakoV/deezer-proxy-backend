13851a1c131e0393e1b259e1b15f6c339facb19d
DmiTriiPetryakoV <petrakovdima199@gmail.com>
2025-10-01 20:47:22 +0300

    Fix CORS for mobile devices

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'https://dmitriipetryakov.github.io',
-  'https://dmitriipetryakov.github.io/musicplaylist/',
+  'https://dmitriipetryakov.github.io/musicplaylist',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  'http://localhost:5173',
+  'http://localhost:5173', 
   'http://localhost:5174',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174'
+  'http://127.0.0.1:5174',
+  'https://dmitriipetryakov.github.io/musicplaylist/'
 ];
-
 
 app.use(cors({
   origin: function (origin, callback) {
